f4bbfea2fd907ada139fab4f507fc49a2e356733
Pieterjan Van Saet <hybridfox2@gmail.com>
2017-05-17 21:29:15 +0200

    Laravel + some index layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="cta">
+        <div class="cta__center">
+            <div class="container">
+                <h1 class="cta__header">
+                    Fan-Tastic: A collection of events from all different kinds of fandom
+                </h1>
+                <h3 class="cta__subheader">
+                    Something here about how great this is
+                </h3>
+                <div class="cta__search">
+                    <input type="text" class="cta__field" placeholder="Search here">
+                    <i class="fa fa-search cta__searchicon"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
